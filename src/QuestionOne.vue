cf8c5ba8ac766014658954b54d5ff1f3e7e217a2
nimesh <nimeshjayasankhanj@gmail.com>
2022-02-11 20:21:18 +0530

    changes

---
+++ src/QuestionOne.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+       <h1>Question One</h1>
+   Title<input type="txt" v-model="title"/>
+   First Name<input type="text" v-model="first_name"/>
+   Last Name<input type="text" v-model="last_name" />
+
+   <p>Welcome {{title}} {{first_name}} {{last_name}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      title:'',
+      first_name:'',
+      last_name:''
+    }
+  },
+  mounted(){
+  },
+  methods:{
+    sum(){
+
+    }
+  }
+}
+</script>
+
+<style>
+</style>
